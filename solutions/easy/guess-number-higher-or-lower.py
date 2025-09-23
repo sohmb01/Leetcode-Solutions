c2b3e5d769f18c531c9d46b134ebf0a03e79988c
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-23 14:59:03 -0400

    Add solution for 374. Guess Number Higher or Lower

--- solutions/easy/guess-number-higher-or-lower.py
+++ solutions/easy/guess-number-higher-or-lower.py
@@ -2,7 +2,7 @@
  * Leetcode Solution for: 374. Guess Number Higher or Lower
  * Difficulty: Easy
  * URL: https://leetcode.com/problems/guess-number-higher-or-lower/submissions/1780535041/
- * Submitted: 2025-09-23T18:58:02.630Z
+ * Submitted: 2025-09-23T18:59:02.655Z
  */
 
 # @return -1 if num is higher than the picked number
