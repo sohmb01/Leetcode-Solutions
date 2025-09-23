1e12f5b3e04fa89e43d19652d8d6f1c9c7502868
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-23 14:35:40 -0400

    Add solution for 4. Median of Two Sorted Arrays

--- solutions/hard/median-of-two-sorted-arrays.py
+++ solutions/hard/median-of-two-sorted-arrays.py
@@ -2,23 +2,9 @@
  * Leetcode Solution for: 4. Median of Two Sorted Arrays
  * Difficulty: Hard
  * URL: https://leetcode.com/problems/median-of-two-sorted-arrays/
- * Submitted: 2025-09-23T18:35:37.737Z
+ * Submitted: 2025-09-23T18:35:37.865Z
  */
 
 class Solution:
-    def thirdMax(self, nums: List[int]) -> int:
-        f,s,t=-999999999999999,-999999999999999,-999999999999999
-        for i in nums:
-            if i>f:
-                s,t=f,s
-                f=i
-                continue
-            if i>s and i!=f:
-                s,t=i,s
-                continue
-            if i>t and i!=s and i!=f:
-                t=i
-        if t==-999999999999999:
-            return f
-        else:
-            return t
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        
