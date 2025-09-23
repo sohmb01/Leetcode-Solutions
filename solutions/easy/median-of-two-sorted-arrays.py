5d1114082ee0c9c420c5e00be95879ae0a9e1b86
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-23 14:35:38 -0400

    Add solution for 414. Third Maximum Number

--- solutions/easy/median-of-two-sorted-arrays.py
+++ solutions/easy/median-of-two-sorted-arrays.py
@@ -2,7 +2,7 @@
  * Leetcode Solution for: 414. Third Maximum Number
  * Difficulty: Easy
  * URL: https://leetcode.com/problems/median-of-two-sorted-arrays/
- * Submitted: 2025-09-23T18:35:36.867Z
+ * Submitted: 2025-09-23T18:35:37.016Z
  */
 
 c
