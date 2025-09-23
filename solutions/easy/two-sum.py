c99e1f9657b19802366fa37198f738b0900b2ed8
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-23 14:26:54 -0400

    Add solution for 1. Two Sum

--- solutions/easy/two-sum.py
+++ solutions/easy/two-sum.py
@@ -1,22 +1,8 @@
 /**
  * Leetcode Solution for: 1. Two Sum
  * Difficulty: Easy
- * URL: https://leetcode.com/problems/two-sum/description/
- * Submitted: 2025-09-23T18:26:49.216Z
+ * URL: https://leetcode.com/problems/two-sum/submissions/1780441267/
+ * Submitted: 2025-09-23T18:26:53.445Z
  */
 
-class Solution:
-    def thirdMax(self, nums: List[int]) 
--> int:
-        f,s,t=-999999999999999,
--999999999999999,-999999999999999
-        for i in nums:
-            if i>f:
-                s,t=f,s
-                f=i
-                continue
-            if i>s and i!=f:
-                s,t=i,s
-                continue
-            if i>t and i!=s and i!=f:
-                t=i
+c
