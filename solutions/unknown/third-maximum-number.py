3694585033dc414ec86c21e382938c3b3c97c5b0
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-23 14:25:38 -0400

    Add solution for Unknown

---
+++ solutions/unknown/third-maximum-number.py
@@ -0,0 +1,24 @@
+/**
+ * Leetcode Solution for: Unknown
+ * Difficulty: Unknown
+ * URL: https://leetcode.com/problems/third-maximum-number/
+ * Submitted: 2025-09-23T18:25:35.691Z
+ */
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        f,s,t=-999999999999999,-999999999999999,-999999999999999
+        for i in nums:
+            if i>f:
+                s,t=f,s
+                f=i
+                continue
+            if i>s and i!=f:
+                s,t=i,s
+                continue
+            if i>t and i!=s and i!=f:
+                t=i
+        if t==-999999999999999:
+            return f
+        else:
+            return t
