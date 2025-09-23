6146bcb1145d2d99fed87a012a1081de7bf116a1
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-23 14:26:58 -0400

    Add solution for 1. Two Sum

--- solutions/easy/two-sum.py
+++ solutions/easy/two-sum.py
@@ -2,7 +2,7 @@
  * Leetcode Solution for: 1. Two Sum
  * Difficulty: Easy
  * URL: https://leetcode.com/problems/two-sum/submissions/1780441267/
- * Submitted: 2025-09-23T18:26:55.203Z
+ * Submitted: 2025-09-23T18:26:58.056Z
  */
 
 c
