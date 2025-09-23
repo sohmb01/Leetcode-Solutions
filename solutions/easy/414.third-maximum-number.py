fb0b8f56d891061b2f73ceda92cce11241a5e871
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-23 16:03:15 -0400

    Add solution for 414. Third Maximum Number

--- solutions/easy/414.third-maximum-number.py
+++ solutions/easy/414.third-maximum-number.py
@@ -2,7 +2,7 @@
  * Leetcode Solution for: 414. Third Maximum Number
  * Difficulty: Easy
  * URL: https://leetcode.com/problems/third-maximum-number/description/
- * Submitted: 2025-09-23T20:03:14.155Z
+ * Submitted: 2025-09-23T20:03:14.791Z
  */
 
 class Solution:
