f3e10bc064d7c7d397916635d879f9656ebb803f
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-24 11:05:33 -0400

    Add solution for 166. Fraction to Recurring Decimal

--- solutions/medium/166.fraction-to-recurring-decimal.py
+++ solutions/medium/166.fraction-to-recurring-decimal.py
@@ -2,7 +2,7 @@
  * Leetcode Solution for: 166. Fraction to Recurring Decimal
  * Difficulty: Medium
  * URL: https://leetcode.com/problems/fraction-to-recurring-decimal/submissions/1781322472/?envType=daily-question&envId=2025-09-24
- * Submitted: 2025-09-24T15:05:31.566Z
+ * Submitted: 2025-09-24T15:05:32.986Z
  */
 
 class Solution:
