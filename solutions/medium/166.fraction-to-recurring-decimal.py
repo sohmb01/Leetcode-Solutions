f78e16996b94ea63710e889d624f12efddf86984
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-24 10:28:51 -0400

    Add solution for 166. Fraction to Recurring Decimal

---
+++ solutions/medium/166.fraction-to-recurring-decimal.py
@@ -0,0 +1,21 @@
+/**
+ * Leetcode Solution for: 166. Fraction to Recurring Decimal
+ * Difficulty: Medium
+ * URL: https://leetcode.com/problems/fraction-to-recurring-decimal/description/?envType=daily-question&envId=2025-09-24
+ * Submitted: 2025-09-24T14:28:50.626Z
+ */
+
+class Solution:
+    def fractionToDecimal(self, numerator: int, denominator: int) -> str:
+        if numerator % denominator == 0:
+            return str(numerator // denominator)
+        
+        negative = (numerator < 0) ^ (denominator < 0)
+        numerator, denominator = abs(numerator), abs(denominator)
+
+        integer_part = numerator // denominator
+        result = "-" if negative else ""
+        result += str(integer_part) + "."
+
+        remainder = numerator % denominator
+        remainders = {}
