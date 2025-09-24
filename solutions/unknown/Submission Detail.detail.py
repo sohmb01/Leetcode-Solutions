56fe8cd2b0804163276129d1e429b272de9694ff
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-24 11:06:08 -0400

    Add solution for Submission Detail

---
+++ solutions/unknown/Submission Detail.detail.py
@@ -0,0 +1,8 @@
+/**
+ * Leetcode Solution for: Submission Detail
+ * Difficulty: Unknown
+ * URL: https://leetcode.com/submissions/detail/1779654413/
+ * Submitted: 2025-09-24T15:06:08.128Z
+ */
+
+
