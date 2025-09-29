ed669123f4330a0d860d3d7746354842b86d9e03
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-29 15:10:40 -0400

    Add solution validate-binary-search-tree

---
+++ solutions/MEDIUM/98.validate-binary-search-tree.py
@@ -0,0 +1,20 @@
+# Problem ID: 98
+# Title: Validate Binary Search Tree
+# Runtime: 1 ms
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def dfs(node,mn,mx):
+            if not node:
+                return True
+            if node.val > mn and node.val<mx:
+                return dfs(node.left,mn,node.val) and dfs(node.right,node.val,mx)
+            else:
+                return False
+        return dfs(root,float('-inf'),float('inf'))
