9f7f3d4c3fe93c2beec7eb3afaf09c3cf9581b66
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-29 15:12:40 -0400

    Add solution range-sum-query-immutable

---
+++ solutions/EASY/303.range-sum-query-immutable.py
@@ -0,0 +1,17 @@
+# Problem ID: 303
+# Title: Range Sum Query - Immutable
+# Runtime: 1080 ms
+
+class NumArray:
+
+    def __init__(self, nums: List[int]):
+        
+        self.nums=nums
+
+    def sumRange(self, left: int, right: int) -> int:
+        return sum(self.nums[left:right+1])
+
+
+# Your NumArray object will be instantiated and called as such:
+# obj = NumArray(nums)
+# param_1 = obj.sumRange(left,right)
