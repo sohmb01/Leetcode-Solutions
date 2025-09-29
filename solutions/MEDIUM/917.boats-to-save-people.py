6bc117c5b3d493e735c262d564fedcbbd9847782
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-29 15:16:03 -0400

    Add solution boats-to-save-people

---
+++ solutions/MEDIUM/917.boats-to-save-people.py
@@ -0,0 +1,15 @@
+# Problem ID: 917
+# Title: Boats to Save People
+# Runtime: 43 ms
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        people.sort()
+        l, r = 0, len(people)-1
+        boats =0 
+        while l<=r:
+            if people[l] + people[r] <= limit:
+                l+=1  
+            r-=1
+            boats+=1
+        return boats
