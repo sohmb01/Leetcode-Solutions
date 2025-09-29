b45b10cef763767db50bc48c3482365c59d05512
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-29 15:19:27 -0400

    Add solution create-hello-world-function

---
+++ solutions/EASY/2809.create-hello-world-function.js
@@ -0,0 +1,17 @@
+// Problem ID: 2809
+// Title: Create Hello World Function
+// Runtime: 54 ms
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
