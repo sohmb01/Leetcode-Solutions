171babdcda8d608b0644fef5db39141e49ba7769
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-29 15:10:42 -0400

    Add solution same-tree

---
+++ solutions/EASY/100.same-tree.py
@@ -0,0 +1,21 @@
+# Problem ID: 100
+# Title: Same Tree
+# Runtime: 24 ms
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+
+class Solution:
+    def isSameTree(self, p: TreeNode, q: TreeNode) -> bool:
+        if (p!=None and q==None)  or (p==None and q!=None ):
+            return False
+        if p==None and q==None :
+            return True
+        else :
+            return p.val==q.val and self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+            
