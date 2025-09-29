314b04859fb1f41889c2ba209a5cda4af52b201e
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-29 15:16:40 -0400

    Add solution remove-sub-folders-from-the-filesystem

---
+++ solutions/MEDIUM/1350.remove-sub-folders-from-the-filesystem.py
@@ -0,0 +1,18 @@
+# Problem ID: 1350
+# Title: Remove Sub-Folders from the Filesystem
+# Runtime: 23 ms
+
+class Solution:
+    def removeSubfolders(self, folder):
+
+        folder.sort()
+        result = [folder[0]]
+
+        for i in range(1, len(folder)):
+            last_folder = result[-1]
+            last_folder += "/"
+            
+            if not folder[i].startswith(last_folder):
+                result.append(folder[i])
+
+        return result
