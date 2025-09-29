db18ba2cef67f217f7d5adb7fecec7020d951645
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-29 15:20:11 -0400

    Add solution find-the-number-of-ways-to-place-people-i

---
+++ solutions/MEDIUM/3278.find-the-number-of-ways-to-place-people-i.py
@@ -0,0 +1,21 @@
+# Problem ID: 3278
+# Title: Find the Number of Ways to Place People I
+# Runtime: 11 ms
+
+class Solution:
+    def numberOfPairs(self, points: List[List[int]]) -> int:
+        points.sort(key = lambda x :(x[0], -x[1]))
+        res = 0
+    
+        for i in range(len(points)-1):
+            x,y = points[i]
+            lower = -1
+            for j in range(i+1, len(points)):
+                if lower < points[j][1] <= y:
+                    res+=1
+                    lower = points[j][1]
+                if lower == y:
+                    break
+        return res
+            
+        return res
