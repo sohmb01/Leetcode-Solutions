ce578f8683fedb91e2d6752cd19ecd4c13ed2649
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-29 15:15:22 -0400

    Add solution binary-search

---
+++ solutions/EASY/792.binary-search.py
@@ -0,0 +1,16 @@
+# Problem ID: 792
+# Title: Binary Search
+# Runtime: 0 ms
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l,r = 0,len(nums)-1
+        while l<=r:
+            mid = l+ (r-l+1)//2
+            if target > nums[mid]:
+                l = mid+1
+            elif target < nums[mid]:
+                r = mid-1
+            else:
+                return mid
+        return -1
