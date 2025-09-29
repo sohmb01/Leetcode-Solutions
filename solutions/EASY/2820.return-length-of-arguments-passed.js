fe4fbb0055bd98a70304a7aef4abc93e7aa8744e
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-29 15:19:33 -0400

    Add solution return-length-of-arguments-passed

---
+++ solutions/EASY/2820.return-length-of-arguments-passed.js
@@ -0,0 +1,15 @@
+// Problem ID: 2820
+// Title: Return Length of Arguments Passed
+// Runtime: 39 ms
+
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+var argumentsLength = function(...args) {
+    return args.length;
+};
+
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
