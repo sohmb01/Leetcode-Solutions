a6a8f08fca999b94405054f303c9d7660326e115
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-29 15:14:47 -0400

    Add solution can-place-flowers

---
+++ solutions/EASY/605.can-place-flowers.py
@@ -0,0 +1,15 @@
+# Problem ID: 605
+# Title: Can Place Flowers
+# Runtime: 15 ms
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        for i in range(len(flowerbed)):
+            left = i == 0 or flowerbed[i-1] == 0
+            right = i == len(flowerbed) - 1 or flowerbed[i+1] == 0
+
+            if left and right and flowerbed[i] == 0:
+                flowerbed[i] = 1
+                n -= 1
+        
+        return n <= 0
