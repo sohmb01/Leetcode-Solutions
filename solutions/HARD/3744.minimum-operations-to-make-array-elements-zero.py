220ae0603c8125560fe56c8b8ee7d4cbfa770216
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-29 15:21:14 -0400

    Add solution minimum-operations-to-make-array-elements-zero

---
+++ solutions/HARD/3744.minimum-operations-to-make-array-elements-zero.py
@@ -0,0 +1,20 @@
+# Problem ID: 3744
+# Title: Minimum Operations to Make Array Elements Zero
+# Runtime: 4402 ms
+
+class Solution:
+    def get(self, num: int) -> int:
+        i = 1
+        base = 1
+        cnt = 0
+        while base <= num:
+            cnt += ((i + 1) // 2) * (min(base * 2 - 1, num) - base + 1)
+            i += 1
+            base *= 2
+        return cnt
+
+    def minOperations(self, queries: List[List[int]]) -> int:
+        res = 0
+        for q in queries:
+            res += (self.get(q[1]) - self.get(q[0] - 1) + 1) // 2
+        return res
