3e2ceedf7b2eba78a139e1e1e899b077088bd823
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-29 15:16:47 -0400

    Add solution maximum-candies-you-can-get-from-boxes

---
+++ solutions/HARD/1424.maximum-candies-you-can-get-from-boxes.py
@@ -0,0 +1,19 @@
+# Problem ID: 1424
+# Title: Maximum Candies You Can Get from Boxes
+# Runtime: 12 ms
+
+class Solution:
+    def maxCandies(self, status: List[int], candies: List[int], keys: List[List[int]], containedBoxes: List[List[int]], initialBoxes: List[int]) -> int:
+        boxes = set(initialBoxes)
+        bfs = [i for i in boxes if status[i]]
+        for i in bfs:
+            for j in containedBoxes[i]:
+                boxes.add(j)
+                if status[j]:
+                    bfs.append(j)
+            for j in keys[i]:
+                if status[j] == 0 and j in boxes:
+                    bfs.append(j)
+                status[j] = 1
+        return sum(candies[i] for i in bfs)
+                
