5dbaa907688929e4ef2f083971ba0ff3a0ca444e
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-29 15:10:24 -0400

    Add solution subsets

---
+++ solutions/MEDIUM/78.subsets.py
@@ -0,0 +1,11 @@
+# Problem ID: 78
+# Title: Subsets
+# Runtime: 0 ms
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        ans = [[]]
+        for num in nums:
+            for i in range(len(ans)):
+                ans.append(ans[i]+[num])
+        return ans
