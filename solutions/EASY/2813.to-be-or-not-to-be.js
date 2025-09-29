88bb67d19e4b4e3e1bb32453db75a2df421db88c
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-29 15:19:34 -0400

    Add solution to-be-or-not-to-be

---
+++ solutions/EASY/2813.to-be-or-not-to-be.js
@@ -0,0 +1,33 @@
+// Problem ID: 2813
+// Title: To Be Or Not To Be
+// Runtime: 62 ms
+
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    return {
+        toBe: (v) => {
+            if (val===v){
+                return true;
+            }
+            else{
+                throw new Error ("Not Equal");
+            }
+        },
+        notToBe: (v) => {
+            if (val!==v){
+                return true;
+            }
+            else{
+                throw new Error ("Equal");
+            }
+        }
+    }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
