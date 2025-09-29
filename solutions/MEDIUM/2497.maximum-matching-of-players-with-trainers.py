f02a0adc70b6d4ba3dc6515fb46eabf89c9b8fe8
Soham <124415246+sohmb01@users.noreply.github.com>
2025-09-29 15:18:45 -0400

    Add solution maximum-matching-of-players-with-trainers

---
+++ solutions/MEDIUM/2497.maximum-matching-of-players-with-trainers.py
@@ -0,0 +1,16 @@
+# Problem ID: 2497
+# Title: Maximum Matching of Players With Trainers
+# Runtime: 75 ms
+
+class Solution:
+    def matchPlayersAndTrainers(self, players: List[int], trainers: List[int]) -> int:
+        players.sort()
+        trainers.sort()
+        p,t = 0,0
+        res = 0
+        while p<len(players) and t<len(trainers):
+            if players[p] <= trainers[t]:
+                res+=1
+                p+=1
+            t+=1
+        return res
